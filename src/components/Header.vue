68c549e1da8bca36b159d7587f003c73832b8868
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-12-20 15:59:40 +0100

    Problema con reset

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        
+    </header>
+</template>
+
+<script>
+export default {
+    neme: "Header"
+}
+</script>
+
+<style scoped>
+    header{
+        width: 100%;
+        height: 50px;
+    
+
+        /* test */
+        background-color: darkcyan;
+    }
+</style>
