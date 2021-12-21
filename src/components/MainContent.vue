1fa5979ba7ccbb569cc5eed5c0691e6370296090
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-12-21 15:44:24 +0100

    Aggiunto main-title

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <span class="main-title">Content goes here</span>
+        <span class="main-title">CURRENT SERIES</span>
     </div>
 </template>
 
@@ -13,8 +13,20 @@ export default {
 <style lang="scss" scoped>
 @import '../style/variables.scss';
 
-    .main-title {
-        font-size: 30px;
-        color: $secondary_color;
+    .container {
+        padding: 50px 0;
+        position: relative;
+
+        .main-title {
+            padding: 8px 30px;
+            font-size: 30px;
+            color: $secondary_color;
+            background-color: $primary_color;
+            position: absolute;
+            top: 0;
+            left: 0;
+            transform: translateY(-50%);
+        }
     }
+
 </style>
