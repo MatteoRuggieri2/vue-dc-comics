4fb3e68a34f9c6c26f009b69ea352d0162b84bb7
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-12-21 16:12:27 +0100

    Corretto posizione dei data, aggiunto SingleSeries

---
+++ src/components/SingleSeries.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>{{ SeriesTitle }}</h2>
+        <h2></h2>
+        <h2></h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SingleSeries',
+    props: ['ImgSrc', 'ImgAlt', 'SeriesTitle']
+}
+</script>
